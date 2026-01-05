157a4b2205c35c4e3ebd96ed8660146a3216a863
nato-git <s.f.h.o1219@gmail.com>
2026-01-05 16:47:17 +0900

    機能の追加、未確認

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 import * as server from '@minecraft/server';
+
+server.system.runInterval(() => {
+  for (const player of server.world.getAllPlayers()) {
+    const inventory = player.getComponent('inventory').container;
+    const selectItem = inventory.getItem(player.selectedSlotIndex);
+    if (!selectItem) {
+      return;
+    } else {
+      selectItem.setLore(selectItem.typeId);
+      inventory.setItem(player.selectedSlotIndex, selectItem);
+    }
+  }
+}, 10);
